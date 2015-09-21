dee752f20e2ae373076685e14146daa95c6f2398
Wesley <root@localhost.localdomain>
2015-09-21 15:55:10 -0400

    removed unneeded Build files

--- Build/CMakeFiles/G4_STORK.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/G4_STORK"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/G4_STORK.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
